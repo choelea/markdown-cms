f136d009d28390297d9d15f8959a8ace9b3e087c
Joe Li <joe.lea@foxmail.com>
2018-03-04 23:04:16 +0800

    change css; copy template into new file when create

--- app/routes/page.create.route.js
+++ app/routes/page.create.route.js
@@ -2,29 +2,33 @@
 'use strict';
 
 // Modules
-var fs           = require('fs');
+var fs = require('fs');
 var get_filepath = require('../functions/get_filepath.js');
 
-function route_page_create (config, raneto) {
+function route_page_create(config, raneto) {
   return function (req, res, next) {
 
-    var filepath = get_filepath({
-      content  : raneto.config.content_dir,
-      category : req.body.category,
-      filename : req.body.name + '.md'
+    var template = get_filepath({
+      content: raneto.config.content_dir,
+      filename: 'template.md'
     });
-
+    var filepath = get_filepath({
+      content: raneto.config.content_dir,
+      category: req.body.category,
+      filename: req.body.name + '.md'
+    });
+    fs.copyFileSync(template, filepath)
     fs.open(filepath, 'a', function (error, fd) {
       if (error) {
         return res.json({
-          status  : 1,
-          message : error
+          status: 1,
+          message: error
         });
       }
       fs.close(fd);
       res.json({
-        status  : 0,
-        message : config.lang.api.pageCreated
+        status: 0,
+        message: config.lang.api.pageCreated
       });
     });
 
