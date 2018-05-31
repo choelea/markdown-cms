63bdbbcc60fe1d7d65b89310cadee021151ceef3
Joe Li <joe.lea@foxmail.com>
2018-05-31 22:26:25 +0800

    change title

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ var fs = require('fs')
 var config = {
 
   // Your site title (format: page_title - site_title)
-  site_title: '旧书 Tech. Blog',
+  site_title: '旧书 - IT Technologies',
 
   // The base URL of your site (can use %base_url% in Markdown files)
   base_url: 'http://localhost:3000',
