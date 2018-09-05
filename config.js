c3e45c5fa4c90e993666a9e10bdb710236275322
Joe Li <joe.li@okchem.com>
2018-09-05 09:56:06 +0800

    change title

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ var fs = require('fs')
 var config = {
 
   // Your site title (format: page_title - site_title)
-  site_title: '旧书 - IT Technologies',
+  site_title: 'OKCHEM - DOC ONLINE',
 
   // The base URL of your site (can use %base_url% in Markdown files)
   base_url: 'http://localhost:3000',
